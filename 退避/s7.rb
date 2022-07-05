d042df3b0517af39287af06c85535749be504165
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-05 10:45:02 +0900

    今日の学習ノート

--- 退避/s7.rb
+++ 退避/s7.rb
@@ -110,4 +110,116 @@ C.new
 S.new
 S.new
 
-p C.class_variable_get(:@@val)
+# p C.class_variable_get(:@@val)
+
+# x = [3,5,3,6,7,2]
+# p x.sort{|a,b| a <=> b}
+
+class CC
+    MSG = "msg1"
+    MSG2 = "msg2"
+    class C2
+        MSG = "C2::msg1"
+        puts MSG
+        puts MSG2
+    end
+    puts MSG
+    puts MSG2
+end
+# 定数の探索はまず自クラス、その次に外側のクラスを探索する。
+
+# C2::msg1
+# msg2
+# msg1
+# msg2
+# p CC::C2.constants
+# p CC.constants
+# p C2.class #=> uninitialized constant C2 (NameError)
+
+# class ClassA
+#     def greet
+#         p "hello from ClassA"
+#     end
+# end
+
+# 下記3つとも同じ意味
+# ClassA.new.greet
+# ::ClassA.new.greet
+# Object::ClassA.new.greet
+
+class ClassA
+    def greet
+        p "Hello from ClassA"
+    end
+    MSG = 'hello'
+
+    class ClassB
+        def greet
+          p  "Hello from ClassA::ClassB"
+        end
+
+        class ClassA
+            def greet
+               p "Hello from ClassA::ClassB::ClassA"
+            end
+        end
+
+        class ClassC
+            def relative
+                ClassA.new.greet
+            end
+
+            def absolute_by_double_colon
+                ::ClassA.new.greet
+            end
+
+            def absolute_by_object
+                Object::ClassA.new.greet
+            end
+
+            def sample
+                p MSG
+            end
+        end
+    end
+end
+
+# ClassA::ClassB::ClassC.new.relative                  #=> "Hello from ClassA::ClassB::ClassA"
+# ClassA::ClassB::ClassC.new.absolute_by_double_colon  #=> "Hello from ClassA"
+# ClassA::ClassB::ClassC.new.absolute_by_object        #=> "Hello from ClassA"
+# ClassA::ClassB::ClassC.new.sample
+
+
+
+class Object
+    private
+    def foo
+        puts 'hello'
+    end
+end
+def foo
+    puts 'hello'
+end
+# トップレベルはオブジェクトクラス。
+# トップレベルで定義されたメソッドはprivate。
+
+# /(\d+)/ =~ "abcd12defgh"
+# puts $1
+
+ary = [1,2,3].freeze
+# ary[1] = 3 #=> can't modify frozen Array: [1, 2, 3] (FrozenError)
+# p ary.object_id #=> 60
+# ary += [4,5]
+# p ary.object_id #=> 80
+# p ary
+
+class Err1 < StandardError; end
+class Err2 < Err1; end
+
+begin
+    Err2
+rescue Err1 => e #=> ここで指定されている例外クラスのサブクラスまでしかレスキューされないらしい。
+    # つまり、StandardErrorでレスキューするならどのエラーが発生してもレスキューされる。
+    puts "Error"
+end
+
