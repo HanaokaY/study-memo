e097cd1e35241953d72938c517fdaedb12a2fc77
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-14 03:39:43 +0900

    requireとloadの違い

---
+++ 模擬試験/test3.rb
@@ -0,0 +1,24 @@
+module M
+    def class_m
+        "class_m"
+    end
+end
+
+class C
+    include M
+end
+
+# p C.methods.include? :class_m
+# Kernelモジュールのmethodsメソッドは、特異メソッドの一覧を取得するメソッド
+# extendは特異メソッドとして追加する
+
+
+
+# requireとloadの違い
+
+# requireは同じファイルは1度のみロードする、loadは無条件にロードする。
+# requireは.rbや.soを自動補完する、loadは補完は行わない。
+# requireはライブラリのロード、=> 1回でいい
+# loadは設定ファイルの読み込みに用いる。 => 都度読み込みたい
+
+
