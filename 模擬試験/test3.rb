a5d212be53f762de3c9bf75fc868948bdf4a6916
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-15 02:20:21 +0900

    Refinementのなかでsuperを実行するとRefinementの対象クラスのメソッドを探索

--- 模擬試験/test3.rb
+++ 模擬試験/test3.rb
@@ -149,11 +149,12 @@ p1.call("9") #=> lambdaだとArgumentError
 
 
 # class C
-#     CONST = "ブロックを渡しているから、外側のスコープがコンテキストになる。これは無視される。"
+#     # CONST = "ブロックを渡しているから、外側のスコープがコンテキストになる。これは無視される。"
 #   end
 #   module M
-#     CONST = "ネストの状態は#{Module.nesting}"
+#     # CONST = "ネストの状態は#{Module.nesting}"
 #     C.class_eval do
+#         CONST = "ネストの状態は#{Module.nesting}"
 #       def awesome_method
 #         CONST
 #       end
@@ -161,3 +162,89 @@ p1.call("9") #=> lambdaだとArgumentError
 #   end
   
 #   p C.new.awesome_method
+
+
+# begin
+#     print "liberty" + :fish.to_s
+#   rescue TypeError
+#     print "TypeError."
+#   rescue
+#     print "Error."
+#   else
+#     print "Else." #=> エラーが発生しなかった場合の処理を行うにはelseを用います。
+#   ensure
+#     print "Ensure."
+#   end
+  
+
+
+mod = Module.new
+mod.module_eval do
+  EVAL_CONST = 100
+end
+p "EVAL_CONST is defined? #{mod.const_defined?(:EVAL_CONST, false)}"
+p "EVAL_CONST is defined? #{Object.const_defined?(:EVAL_CONST, false)}"
+
+
+
+# TOPCONST = 'トップレベルの定数だよ'
+# class C
+#     # CCONST = 'Cの定数だよ'
+#   end
+  
+#   # ブロックの場合スコープはclass_evalの外側になる。この場合トップレベル
+#   C.class_eval do
+#     p Module.nesting # ネストの状態はトップレベル
+#     # => []
+#     p TOPCONST
+#     CCONST = 'Cの定数だよ'
+#     # => "トップレベルの定数だよ"
+#     # p CCONST # クラスCはスコープ外
+#     # => NameError
+#   end
+
+
+#   p Object.const_defined?(:CCONST, false)
+#   p C.const_defined?(:CCONST, false)
+
+# --------------------------------------------------------------------------------
+# ブロックを渡した場合は、
+# eval内で定数を定義しても、評価しているクラス内に定義されるわけではなく、
+# 外側のスコープ内で定義されることになる。
+# --------------------------------------------------------------------------------
+
+
+
+
+
+
+class C
+end
+
+module M
+  refine C do
+    def m1(value)
+      p "define m1 using Refinement"
+      super value - 100 # 300 - 100
+    end
+  end
+end
+
+class C
+  def m1(value)
+    p "define m1 in C"
+    value - 100 # 200 - 100
+  end
+end
+
+using M # ここからRefinementが有効になる
+
+class K < C
+  def m1(value)
+    p "define m1 in K"
+    super value - 100 # 400 - 100
+    # Refinementが有効なのでsuperはモジュールMにあるm1を参照する
+  end
+end
+
+puts K.new.m1 400
