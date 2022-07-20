1301a8822aad9fe2d1268f08cc0a1e9ae0f15612
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-21 01:47:47 +0900

    今日の学習ノート

--- 模擬試験/test13.rb
+++ 模擬試験/test13.rb
@@ -7,12 +7,15 @@ module Inc
 end
 
 class C
-    include Inc
-    # prepend Pre
-    extend Pre
+    # include Inc
+    # # prepend Pre
+    # extend Pre
+    require Inc
 end
 
 p C.singleton_class.ancestors
 # includeとprependだと特異クラスの継承チェーンにはでてこない。つまり、クラスメソッドは取り込めていない。
 # extendだと特異クラスの継承チェーンに出てくる。つまり、クラスメソッドとして取り込んでいる
+
+
 
