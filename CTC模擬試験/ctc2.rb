4fc62cd785d9243b04437f0d7f5eee82363a359c
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-23 06:18:09 +0900

    盲点

--- CTC模擬試験/ctc2.rb
+++ CTC模擬試験/ctc2.rb
@@ -1,39 +1,46 @@
 
-[コード]
-class Foo
-    def initialize(obj)
-        obj.foo
-    end
+# # [コード]
+# class Foo
+#     def initialize(obj)
+#         # obj.fof #=> undefined method `fof' for #<Bar:0x00007fd230125200> (NoMethodError)
+#         obj.foo
+#     end
 
-    def foo
-        puts "foofoofoo"
-    end
-end
+#     def foo
+#         puts "foofoofoo"
+#     end
+# end
 
-class Bar
-    def foo
-        puts "barbarbar"
-    end
-end
+# class Bar
+#     def foo
+#         puts "barbarbar"
+#     end
+# end
 
-Foo.new(Bar.new)
+# Foo.new(Bar.new) #=> barbarbar
 
 
-[コード]
-class Bar
-    def foo
-        puts "barbarbar"
-    end
-end
+# # [コード]
+# class Bar
+#     def foo
+#         puts "barbarbar"
+#     end
+# end
 
-class Foo < Bar
-    def initialize(obj)
-        obj.foo
-    end
+# class Foo < Bar
+#     def initialize(obj)
+#         obj.foo
+#     end
 
-    def foo
-        puts "foofoofoo"
-    end
-end
+#     def foo
+#         puts "foofoofoo"
+#     end
+# end
 
-Foo.new(Foo.new(Bar.new))
+# Foo.new(Foo.new(Bar.new))
+
+
+
+# 感想
+
+# 引数にオブジェクトを渡して、オブジェクトの特異メソッドを実行することってできたんだな〜って感じ。
