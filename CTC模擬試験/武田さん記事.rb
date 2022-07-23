d39fc744d09fc2546b2ed25f51a2b013ca78fbd4
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-24 01:46:11 +0900

    定数はメソッド内で更新、定義はできない

--- CTC模擬試験/武田さん記事.rb
+++ CTC模擬試験/武田さん記事.rb
@@ -16,3 +16,43 @@
 # inspectメソッドはpメソッドでオブジェクトそのものを出力した際の文字列表現を指定するメソッドです。
 
 
+# ensureの値は無視される
+# 解説
+# beginもensurenも各スコープ内でp,puts,print等の出力メソッドを使えば、出力は実行されるが
+# 値については挙動が変わる
+
+# begin節の値に関してはensureまでの出力が終わったら、最後に戻り値として返されるけど、
+# ensureに関しては値が無視される。(メソッドの戻り値として返される。)
+
+def foo
+    begin
+        true        
+    ensure
+        false #=> ensure節では簡単な例外処理を実装する程度が望ましい
+        # 実際にこの節の値は戻り値とはならない
+    end
+end
+
+# p foo
+
+
+# 定数をメソッド内で更新、定義することはできない
+
+class Cls1
+    COSNT = 'HELLO' #=> 当然、問題なく定義できる
+    def foo
+        # COSNT = 'Class in HELLO'  #=> メソッドを定義した時点でエラーとなる
+    end
+    def fee(xxx)
+        # COSNT = xxx #=> こちらもエラーになる
+    end
+    # しかし、<<(concat)を使用することは問題がない
+    def faa
+        CONST << 'hello' #=> <<は+=ではないから、なんの問題もないらしい
+    end
+end
+
+
+
+
+
