0ec7d30848ccaa047a665f77a723da1e71311f7d
HanaokaY <okayu5757@ezweb.ne.jp>
2022-08-07 02:54:30 +0900

    引数の段階でも可能

--- React学習/sample-app/src/components/ColoredMessage.jsx
+++ React学習/sample-app/src/components/ColoredMessage.jsx
@@ -21,12 +21,22 @@
 
 // さらに、分割代入したことにより省略記法を使用することが可能になった。
 
-export const ColoredMessage = (props) => {
-    const {color,children} = props;
+// export const ColoredMessage = (props) => {
+//     const {color,children} = props;
+//     const contentBlueStyle = {
+//         color, // プロパティ名と設定値が同一の場合、設定値のみに省略することが可能になる。
+//         fontSize: "20px"
+//     };
+//     return <p style={contentBlueStyle}>{children}</p>
+// };
+
+
+// 引数の段階で分割代入と省略記法どちらもできる
+
+export const ColoredMessage = ({color,children}) => { // そもそも引数にはpropsが渡ってきているから、この段階でも可能
     const contentBlueStyle = {
-        color, // プロパティ名と設定値が同一の場合、設定値のみに省略することが可能になる。
+        color,
         fontSize: "20px"
     };
     return <p style={contentBlueStyle}>{children}</p>
 };
-
