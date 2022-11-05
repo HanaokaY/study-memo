f7dcd7b4a6b26b9b8dca253cbc3db1db521f669a
HanaokaY <okayu5757@ezweb.ne.jp>
2022-11-05 14:59:38 +0900

    &というキーワードが「値のポインタ(メモリアドレス)を取得する」という演算子

--- Go学習/go-qr-app/main.go
+++ Go学習/go-qr-app/main.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"flag"
     "fmt"
-	"os"
 )
 
 func main() {    
-	flag.Parse()
-	msg := fmt.Sprintf("Hello %s\n", flag.Arg(0))
-
-	f, err := os.Create("./hello.txt")
-	if err != nil {
-		fmt.Printf("failed to create file \n: %v", err)
-		return
-	}
-	defer f.Close()
-	// defer fmt.Println("mianの処理が全部終わったあとに呼ばれる")
-
-	_, err = f.WriteString(msg) // ここでは変数の宣言ではなく、代入しているから:=ではなく、ただの=
-	if err != nil{
-		fmt.Printf("failed to write message to file \n: %v", err)
-		return
-	}
-
-	fmt.Println("Done!")
+	a := "hello"
+	fmt.Println(&a)
+	// &というキーワードが「値のポインタ(メモリアドレス)を取得する」という演算子
 }
 
